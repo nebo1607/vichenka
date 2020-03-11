2fdbcbb8b5f9250a3f6d562126a1ee29c2ac6c51
alex82529891 <alex.neborochko@gmail.com>
2020-03-11 12:30:18 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+console.log("Bonjour");
+
+// type string: var foo = "ceci est une string";
+
+// type number: var bar = 4; 
+
+// type boolean: var bol = true;  var bol = false;
+
+// type undefined: var nomdemavariable;
+
+//type null:  var variable = null;
+
+//type array: (tableau) var tableau1 = ["red", "orange","blue"];
+
+//type object: var objectType = {"Nom":"nebo", "Prénom": "alex", "Age":"27"}
+
+    
+//console.log(objectType);
+
+//type Fonction
+//var variableDeTypeFunction = function(){
+ //   return "string returnée"}
+
+//var test= "ceci est un test"
+//var test= "caci est un deuxieme test"
+
+//console.log(typeof foo);
+
+//var header = document.getElementsByTagName('li');
+//var selectionParClasse = document.getElementsByClassName("li");
+//var nimporteQuoi = document.getElementsByClassName("id_test");
+
+//console.log(nimporteQuoi);
+
+//var gloubiboulga = document.createElement("div");
+//document.body.appendChild(gloubiboulga)
+
+
+
+//gloubiboulga.classList.add("foo","bar");
+//document.body.appendChild(gloubiboulga);
+
+
+
+
+
